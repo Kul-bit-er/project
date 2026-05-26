7238a75b1d35a23246a2e76e0048f36662990d79
Kul-bit-er <fairytail.animka@gmail.com>
2026-05-26 12:46:45 +0300

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('Скрипт работает корректно')
+});
+
+const contactButtonModal = document.querySelector(".contact-us");
+const modalApplication = document.querySelector(".applications");
+const closeModalButton = document.querySelector(".application-close");
+
+if (contactButtonModal && modalApplication) {
+    contactButtonModal.addEventListener("click", function(event) {
+          modalApplication.removeAttribute("hidden");
+        }
+    );
+}
+window.addEventListener("click", function(event) {
+    if (event.target === modalApplication) {
+        modalApplication.setAttribute("hidden", true);
+    }
+});
+
+closeModalButton.addEventListener("click", function(event)  {
+    modalApplication.setAttribute("hidden", true);
+});
+
+    
